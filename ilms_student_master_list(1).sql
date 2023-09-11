54da2a132a061a2016dd2a39f89255214a9051ab
jhom <jhomignacio08@gmail.com>
2023-09-11 10:38:17 +0800

    added db

--- ilms_student_master_list(1).sql
+++ ilms_student_master_list(1).sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Sep 09, 2023 at 10:56 AM
--- Server version: 8.0.30
--- PHP Version: 8.1.10
+-- Generation Time: Sep 11, 2023 at 02:37 AM
+-- Server version: 5.7.33
+-- PHP Version: 7.4.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,8 +28,8 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `log` (
-  `id` int NOT NULL,
-  `user_id` int NOT NULL,
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
   `action` varchar(200) NOT NULL,
   `date_created` timestamp NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -41,15 +41,15 @@ CREATE TABLE `log` (
 --
 
 CREATE TABLE `master_list` (
-  `id` int NOT NULL,
-  `student_number` int NOT NULL,
-  `status` int NOT NULL,
+  `id` int(11) NOT NULL,
+  `student_number` varchar(200) NOT NULL,
+  `status` int(11) NOT NULL,
   `first_name` varchar(100) NOT NULL,
   `last_name` varchar(100) NOT NULL,
   `middle_name` varchar(100) NOT NULL,
   `gender` varchar(100) NOT NULL,
   `dob` date NOT NULL,
-  `age` int NOT NULL,
+  `age` int(11) NOT NULL,
   `contact` varchar(100) NOT NULL,
   `address` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
@@ -66,9 +66,9 @@ CREATE TABLE `master_list` (
 --
 
 INSERT INTO `master_list` (`id`, `student_number`, `status`, `first_name`, `last_name`, `middle_name`, `gender`, `dob`, `age`, `contact`, `address`, `email`, `college`, `year_level`, `program`, `major`, `site_name`, `created_at`) VALUES
-(6, 2015105100, 1, 'jhom', 'ignacio', 'luanzon', 'male', '2023-09-09', 25, '0935298979', 'Barangay Care Tarlac City', 'jhomigancio@mail.com', 'ccs', '2', 'BSIT', 'WMA', 'http://ilmsapi.test/signup', '2023-09-09 04:26:31'),
-(7, 2015105200, 0, 'jhom', 'ignacio', 'luanzon', 'male', '2023-09-01', 33, '0935298979', 'Barangay Care Tarlac City', 'jdev.netseek@gmail.com', 'ccs', '3', 'BSIT', 'WMA', 'http://ilmsapi.test/signup', '2023-09-09 08:48:00'),
-(8, 2015105201, 0, 'rap', 'test', 'test', 'female', '2023-09-23', 25, '0935298979', 'Barangay Care Tarlac City', 'jhomignacio08@gmail.com', 'ccs', '5', 'BSIT', 'WMA', 'http://ilmsapi.test/signup', '2023-09-09 09:10:44');
+(6, '201510510050', 1, 'jhomEL', 'ignacio', 'luanzon', 'male', '2023-09-09', 25, '0935298979', 'Barangay Care Tarlac City', 'jhomigancio@mail.com', 'ccs', '2', 'BSIT', 'WMA', 'http://ilmsapi.test/signup', '2023-09-09 04:26:31'),
+(7, '2015105200', 1, 'jhom', 'ignacio', 'luanzon', 'male', '2023-09-01', 33, '0935298979', 'Barangay Care Tarlac City', 'jdev.netseek@gmail.com', 'ccs', '3', 'BSIT', 'WMA', 'http://ilmsapi.test/signup', '2023-09-09 08:48:00'),
+(8, '2015105201', 1, 'rap', 'test', 'test', 'female', '2023-09-23', 25, '0935298979', 'Barangay Care Tarlac City', 'jhomignacio08@gmail.com', 'ccs', '5', 'BSIT', 'WMA', 'http://ilmsapi.test/signup', '2023-09-09 09:10:44');
 
 -- --------------------------------------------------------
 
@@ -77,10 +77,11 @@ INSERT INTO `master_list` (`id`, `student_number`, `status`, `first_name`, `last
 --
 
 CREATE TABLE `users` (
-  `id` int NOT NULL,
+  `id` int(11) NOT NULL,
+  `role` varchar(100) DEFAULT NULL,
   `email` varchar(200) NOT NULL,
   `username` varchar(100) NOT NULL,
-  `password` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `password` varchar(255) NOT NULL,
   `password_token` varchar(200) NOT NULL,
   `password_token_exp` varchar(200) NOT NULL,
   `created_at` timestamp NOT NULL
@@ -90,8 +91,8 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `email`, `username`, `password`, `password_token`, `password_token_exp`, `created_at`) VALUES
-(1, 'jhomignacio08@gmail.com', 'admin', '$2y$10$OACqzHWMT84X2ykfd1PXJumhOc9B7cVl/JbDsI1xNCXHbl8kcs8pa', '1b5f7be96095efa91eea8cde49464ca71ae692ba2281362e3b21debcd3a0c4fb', '2023-09-09 10:21:37', '2023-08-24 08:27:40');
+INSERT INTO `users` (`id`, `role`, `email`, `username`, `password`, `password_token`, `password_token_exp`, `created_at`) VALUES
+(1, 'admin', 'jhomignacio08@gmail.com', 'admin', '$2y$10$mCEdm1JGRglGM92egDCaO.DGDylZ6ZDDjEID0STkkLlYN9mYDdx3K', 'c857c637cf00b401a4b0298c2e943bbc95a92a51f966414592f3ba23c2bae1e0', '2023-09-10 10:31:36', '2023-08-24 08:27:40');
 
 --
 -- Indexes for dumped tables
@@ -107,8 +108,7 @@ ALTER TABLE `log`
 -- Indexes for table `master_list`
 --
 ALTER TABLE `master_list`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `student_number` (`student_number`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -124,19 +124,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `log`
 --
 ALTER TABLE `log`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `master_list`
 --
 ALTER TABLE `master_list`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- views/auth/register.php
+++ views/auth/register.php
@@ -106,10 +106,12 @@
              
                 <div class="_input_container">
                   <div class="mt-2">
-                    <select name="college" id="" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                  <input  name="college" type="text" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+
+                    <!-- <select name="college" id="" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                       <option value="ccs">College of Computer Studies</option>
                       <option value="coed">College of Education</option>
-                    </select>
+                    </select> -->
                   </div>
                 </div>
             </section>
